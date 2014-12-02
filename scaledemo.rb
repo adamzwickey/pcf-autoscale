e7c51abd69214b5e06c2cd6cb3d264fe8dcb8172
azwickey-pivotal <azwickey@gopivotal.com>
2014-12-02 11:53:36 -0500

    Making code compatible with running outside CF

--- scaledemo.rb
+++ scaledemo.rb
@@ -5,14 +5,15 @@
   require 'json'
 
   set :requests, 0
+  set :bind, '0.0.0.0'
 
   get '/' do
      settings.requests += 1     
-     app_info = JSON.parse(ENV['VCAP_APPLICATION'])     
+     app_info = ENV['VCAP_APPLICATION'] ? JSON.parse(ENV['VCAP_APPLICATION']) : Hash.new      
      ENV['APP_NAME'] = app_info["application_name"]
      ENV['APP_INSTANCE'] = app_info["instance_index"].to_s
-     ENV['APP_MEM'] = app_info["limits"]["mem"].to_s
-     ENV['APP_DISK'] = app_info["limits"]["disk"].to_s
+     ENV['APP_MEM'] = app_info["limits"] ? app_info["limits"]["mem"].to_s : " "
+     ENV['APP_DISK'] = app_info["limits"] ? app_info["limits"]["disk"].to_s : " "
      ENV['APP_IP'] = IPSocket.getaddress(Socket.gethostname)
      ENV['APP_PORT'] = app_info["port"].to_s
      erb :'index'
@@ -24,10 +25,11 @@
   
   get '/load' do
     i = 0
+    myStr = "Kill the CPU!!!"
     buff = ""
 
-    while i < 1000  do
-      buff += i.to_s
+    while i < 50000  do
+      buff += myStr.to_s
       buff.reverse!
       i += 1
     end
